ee4be5d209cf7dd41caae7417253154b1c131529
CleitsonLima <cleitson.ferreira@aluno.senai.br>
2022-07-04 10:46:19 -0300

    first commit

---
+++ CursoGitHub.InterfaceConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoGitHub.InterfaceConsole
+{//inicio do namespace;
+
+    internal class Program
+    {//inicio da classe;
+        static void Main(string[] args)
+        {//inicio do metodo;
+
+            /*
+             * Main - Principal
+             * o método inicia-se com a letra maiscula
+             * 
+             */
+
+            Console.WriteLine("Alô Mundo!");
+            Console.ReadKey();
+
+
+
+
+        }//fim do metodo;
+    }//fim da classe;
+}//fim do namespace;

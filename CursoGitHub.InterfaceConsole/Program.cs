94acabce7246577467af8e107216bd57950189fb
CleitsonLima <cleitson.ferreira@aluno.senai.br>
2022-07-04 11:57:00 -0300

    bem vindo ao c#

--- CursoGitHub.InterfaceConsole/Program.cs
+++ CursoGitHub.InterfaceConsole/Program.cs
@@ -17,10 +17,30 @@ namespace CursoGitHub.InterfaceConsole
              * o método inicia-se com a letra maiscula
              * 
              */
+            //Variavel
+            /*
+             * e um espaco reservado na memoria do computador
+            */
 
+            string pergunta;
+            string resposta="git init";
+
+            Console.WriteLine("Qual comando para iniciar o Git");
+            pergunta = Console.ReadLine();
+            if (pergunta.Equals(resposta))
+            {
+                Console.WriteLine("Acertou");
+            }
+            else 
+            {
+                Console.WriteLine("Errou");
+            }
+            Console.WriteLine($"A resposta é {resposta}");
+
+            /*
             Console.WriteLine("Alô Mundo!");
             Console.ReadKey();
-
+            */
 
 
 
